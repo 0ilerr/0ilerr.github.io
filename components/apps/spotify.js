196bd86f74ae73edc349a89859cbce3d889186ac
0ilerr <euler.giachini@hotmail.com>
2024-01-07 04:03:24 -0300

    Linux model 1 -test

---
+++ components/apps/spotify.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Spotify() {
+    return (
+        <iframe src="https://open.spotify.com/playlist/37i9dQZEVXbMDoHDwVN2tF?si=d6c657ef1cf04187" frameBorder="0" title="Spotify" className="h-full w-full bg-ub-cool-grey"></iframe>
+    )
+}
+
+export const displaySpotify = () => {
+    <Spotify> </Spotify>
+}
